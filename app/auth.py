9f8669a28cfe245fc2d7d4034d5d2c633539f8e4
Max Foxley-Marrable <22856173+Max-FM@users.noreply.github.com>
2022-11-01 19:04:23 -0400

    Add signup functionality

--- app/auth.py
+++ app/auth.py
@@ -1,9 +1,11 @@
-from flask import Blueprint, render_template, redirect
+from flask import Blueprint, render_template, redirect, request, flash
+from . import db
+from .models import Users
 
 auth = Blueprint("auth", __name__)
 
 
-@auth.route("/login")
+@auth.route("/login", methods=['GET', 'POST'])
 def login():
     return render_template("login.html")
 
@@ -13,6 +15,48 @@ def logout():
     return redirect("/")
 
 
-@auth.route("/signup")
+@auth.route("/signup", methods=['GET', 'POST'])
 def signup():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        password_confirm = request.form.get("password_confirm")
+
+        exists = db.session.query(
+            Users.id
+        ).filter_by(
+            username=username
+        ).first() is not None
+
+        if len(username) < 4:
+            flash(
+                "Username must be at least 4 characters long!",
+                category="error"
+            )
+        elif len(password) < 4:
+            flash(
+                "Password must be at least 4 characters long!",
+                category="error"
+            )
+        elif exists:
+            flash(
+                "Username already exists!",
+                category="error"
+            )
+        elif password != password_confirm:
+            flash(
+                "Passwords do not match!",
+                category="error"
+            )
+        else:
+            flash(
+                "Account created!",
+                category="success"
+            )
+            new_user = Users(username=username, password=password)
+            db.session.add(new_user)
+            db.session.commit()
+
+            return redirect("/")
+
     return render_template("signup.html")
--- app/models.py
+++ app/models.py
@@ -6,7 +6,7 @@ from datetime import datetime
 class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(16), nullable=False, unique=True)
-    password = db.Column(db.String(16), nullable=False)
+    password = db.Column(db.String(128), nullable=False)
 
 
 class Tasks(db.Model):

73613eae84c4ddce09569bc1e144523260d4f042
Max Foxley-Marrable <22856173+Max-FM@users.noreply.github.com>
2022-11-01 17:28:30 -0400

    Add user class and generate database automatically

--- app/__init__.py
+++ app/__init__.py
@@ -16,7 +16,18 @@ def create_app():
 
     from .views import views
     from .auth import auth
+
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
+    from .models import Users, Tasks  # noqa: F401
+    create_database(app)
+
     return app
+
+
+def create_database(app):
+    if not os.path.exists(DB_PATH):
+        with app.app_context():
+            db.create_all()
+        print('Database created!')
--- app/models.py
+++ app/models.py
@@ -3,7 +3,13 @@ from . import db
 from datetime import datetime
 
 
-class Todo(db.Model):
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(16), nullable=False, unique=True)
+    password = db.Column(db.String(16), nullable=False)
+
+
+class Tasks(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
